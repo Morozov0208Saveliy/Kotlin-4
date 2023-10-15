999f7fcbb68fc975fd8cb99ad2e6981af5cf8739
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-10-15 15:03:45 +0200

    Any needs a cast

--- src/main/kotlin/ru/otus/homework/Collections.kt
+++ src/main/kotlin/ru/otus/homework/Collections.kt
@@ -1,17 +1,20 @@
 package ru.otus.homework
 
 fun main() {
-    val iBox: GenericBox<Int> = createBox(10)
-    val i: Int = iBox.value
+    val iBox: GenericBox = createBox(10)
+    val iAnyValue: Any = iBox.value
+    val i: Int = iAnyValue as Int
 
-    val fBox: GenericBox<Float> = createBox(10F)
-    val f: Float = fBox.value
+    val fBox: GenericBox = createBox(10F)
+    val fAnyValue: Any = fBox.value
+    val f: Float = fAnyValue as Float
 
-    val sBox: GenericBox<String> = createBox("Ten")
-    val s: String = sBox.value
+    val sBox: GenericBox = createBox("Ten")
+    val sAnyValue: Any = sBox.value
+    val s: String = sAnyValue as String
 
     println("${i.toString()}, $f, $s")
 }
 
-data class GenericBox<D>(val value: D)
-fun <D> createBox(value: D): GenericBox<D> = GenericBox(value)
+data class GenericBox(val value: Any)
+fun createBox(value: Any): GenericBox = GenericBox(value)
