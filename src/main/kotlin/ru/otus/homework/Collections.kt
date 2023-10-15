6b19bf1fdb748c54938af2a9cf18b8d6b9b50ff7
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-10-15 14:55:23 +0200

    Types are explicit

--- src/main/kotlin/ru/otus/homework/Collections.kt
+++ src/main/kotlin/ru/otus/homework/Collections.kt
@@ -1,9 +1,16 @@
 package ru.otus.homework
 
 fun main() {
-    println(createBox(10))
-    println(createBox(10F))
-    println(createBox("Ten"))
+    val iBox: GenericBox<Int> = createBox(10)
+    val i: Int = iBox.value
+
+    val fBox: GenericBox<Float> = createBox(10F)
+    val f: Float = fBox.value
+
+    val sBox: GenericBox<String> = createBox("Ten")
+    val s: String = sBox.value
+
+    println("${i.toString()}, $f, $s")
 }
 
 data class GenericBox<D>(val value: D)
