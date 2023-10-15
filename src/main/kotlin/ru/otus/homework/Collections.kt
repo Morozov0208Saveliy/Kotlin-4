056c1027808105b3345a04f5d3fc607ae9fc3139
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-10-15 14:47:13 +0200

    Generic box

--- src/main/kotlin/ru/otus/homework/Collections.kt
+++ src/main/kotlin/ru/otus/homework/Collections.kt
@@ -6,11 +6,5 @@ fun main() {
     println(createBox("Ten"))
 }
 
-data class IntBox(val value: Int)
-fun createBox(int: Int): IntBox = IntBox(int)
-
-data class FloatBox(val value: Float)
-fun createBox(flt: Float): FloatBox = FloatBox(flt)
-
-data class StringBox(val value: String)
-fun createBox(str: String): StringBox = StringBox(str)
+data class GenericBox<D>(val value: D)
+fun <D> createBox(value: D): GenericBox<D> = GenericBox(value)
