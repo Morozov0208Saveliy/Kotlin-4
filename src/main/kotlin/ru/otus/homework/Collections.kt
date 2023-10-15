927e517ef95f64592bdd9672df56bdf49a7de377
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2023-10-15 12:57:35 +0200

    Typed boxes

--- src/main/kotlin/ru/otus/homework/Collections.kt
+++ src/main/kotlin/ru/otus/homework/Collections.kt
@@ -2,7 +2,15 @@ package ru.otus.homework
 
 fun main() {
     println(createBox(10))
+    println(createBox(10F))
+    println(createBox("Ten"))
 }
 
 data class IntBox(val value: Int)
 fun createBox(int: Int): IntBox = IntBox(int)
+
+data class FloatBox(val value: Float)
+fun createBox(flt: Float): FloatBox = FloatBox(flt)
+
+data class StringBox(val value: String)
+fun createBox(str: String): StringBox = StringBox(str)
